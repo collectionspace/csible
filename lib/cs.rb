1a17a33c833f8666778c3f0d343850c1f4f67273
Mark Cooper <markchristophercooper@gmail.com>
2015-04-17 09:24:23 -0700

    Use generic command wrapper and add delete

--- lib/cs.rb
+++ lib/cs.rb
@@ -1,9 +1,12 @@
 # helpers
-def get_command(base, path, params = nil)
-    command = base + " --extra-vars='path=#{path}"
-    command = "#{command} params=#{params}" if params
-    command = "#{command}'"
-    command
+
+# ansible playbook command wrapper
+# type is one of = path, file, directory, url
+def command(base, method, type, arg, params = nil)
+  command = base + " --extra-vars='method=#{method} #{type}=#{arg}"
+  command = "#{command} params=#{params}" if params
+  command = "#{command}'"
+  command
 end
 
 namespace :cs do
@@ -16,12 +19,24 @@ namespace :cs do
     pp JSON.parse( IO.read('api.json') )
   end
 
-  # rake cs:get[/locationauthorities]
-  task :get, [:path, :params] do |t, args|
-    path   = args[:path]
-    params = args[:params] || nil
+  namespace :get do
 
-    sh get_command(base_command, path, params)
+    # rake cs:get:path[/locationauthorities]
+    task :path, [:path, :params] do |t, args|
+      path   = args[:path]
+      params = args[:params] || nil
+
+      sh command(base_command, 'GET', 'path', path, params)
+    end
+
+    # rake cs:get:url[https://cspace.lyrasistechnology.org/cspace-services/locationauthorities]
+    task :url, [:url, :params] do |t, args|
+      url    = args[:url]
+      params = args[:params] || nil
+
+      sh command(base_command, 'GET', 'url', url, params)
+    end
+
   end
 
   namespace :post do
@@ -41,12 +56,33 @@ namespace :cs do
       end
     end
 
-    # rake cs:post:directory[/locationauthorities/XYZ/items,examples/locations/1.xml]
+    # rake cs:post:file[/locationauthorities/XYZ/items,examples/locations/1.xml]
     task :file, [:path, :file] do |t, args|
       file = args[:file]
       raise "Invalid file" unless File.file? file
       puts file
       # sh "ansible-playbook -i ..."
+    end
+
+  end
+
+  namespace :delete do
+
+    task :path, [:path] do |t, args|
+      path = args[:path]
+      sh command(base_command, 'DELETE', 'path', path)
+    end
+
+    task :url, [:url] do |t, args|
+      url = args[:url]
+      sh command(base_command, 'DELETE', 'url', url)
+    end
+
+    # rake cs:delete:file[deletes.txt]
+    task :file, [:file] do |t, args|
+      file = args[:file]
+      raise "HELL" unless File.file? file
+      sh command(base_command, 'DELETE', 'file', file)
     end
 
   end
