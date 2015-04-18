d3d1ccc581fe914fe4f25ea86ee944a4b61423df
Mark Cooper <markchristophercooper@gmail.com>
2015-04-17 17:18:34 -0700

    Templates improve namespacing

--- lib/template.rb
+++ lib/template.rb
@@ -4,34 +4,41 @@ namespace :template do
 
   output_dir = 'imports'
 
-  namespace :locationauthorities do
-    template_file = 'templates/locations/item.xml.erb'
-
-    # todo: process *_fields from config file?
-    required_fields = [
-      "shortidentifier",
-      "termname",
-      "locationtype",
-    ]
-
-    optional_fields = [
-      "address",
-      "accessnote",
-      "conditionnote",
-      "securitynote",
-    ]
-
-    filename_fields = [ "shortidentifier" ]
-
-    # rake template:locationauthorities:fields
-    task :fields do |t|
-      print_fields required_fields, optional_fields
+  namespace :locations do
+    namespace :authorities do
+      # todo
+      # rake template:locations:authorities:process[templates/locations/authorities.example.csv]
     end
 
-    # rake template:locationauthorities:process[templates/locations/onsite.csv]
-    task :process, [:csv] do |t, args|
-      process_csv(args[:csv], output_dir, template_file, required_fields, filename_fields)
-    end
+    namespace :items do
+      template_file = 'templates/locations/item.xml.erb'
 
+      # todo: process *_fields from config file?
+      required_fields = [
+        "shortidentifier",
+        "termname",
+        "locationtype",
+      ]
+
+      optional_fields = [
+        "address",
+        "accessnote",
+        "conditionnote",
+        "securitynote",
+      ]
+
+      filename_fields = [ "shortidentifier" ]
+
+      # rake template:locations:items:fields
+      task :fields do |t|
+        print_fields required_fields, optional_fields
+      end
+
+      # rake template:locations:items:process[templates/locations/onsite.csv]
+      task :process, [:csv] do |t, args|
+        process_csv(args[:csv], output_dir, template_file, required_fields, filename_fields)
+      end
+
+    end
   end
 end
