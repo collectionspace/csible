d507abe0ee61ad3e5738bfd4956139c665e9faf0
Mark Cooper <markchristophercooper@gmail.com>
2015-06-10 15:40:53 -0700

    Task to add uri to update csv using redis

--- lib/cs.rb
+++ lib/cs.rb
@@ -259,6 +259,26 @@ namespace :cs do
   namespace :update do
     output_dir = "tmp"
 
+    # rake cs:update:generate[templates/updates/update-id.example.csv,tmp.csv]
+    desc "Generate request csv from csv containing id w/o uri using redis"
+    task :generate, [:csv, :output] do |t, args|
+      csv             = args[:csv]
+      generated       = []
+      output_filename = args[:output] || "response.csv"
+      redis           = Redis.new
+      raise "HELL" unless File.file? csv
+      CSV.foreach(csv, {
+          headers: true, :header_converters => :symbol, :converters => [:nil_to_empty]
+        }) do |row|
+        data = row.to_hash
+        uri  = redis.get( data[:id] )
+        raise "Invalid id #{data[:id]}" unless uri
+        data[:uri] = uri
+        generated << data
+      end
+      write_csv(output_filename, generated)
+    end
+
     # rake cs:update:nested[templates/updates/update-nested.example.csv]
     desc "Update requests by csv with nested template"
     task :nested, [:csv] do |t, args|
