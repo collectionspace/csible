edcb2a2b3ff21c6d8f827e495e46fad7468c3699
Richard Millet <remillet@yahoo.com>
2015-12-09 22:25:41 -0800

    NOJIRA: Renamed sql file that updated the General Subject Terms (concentConcept) field.

--- rake-update.sh
+++ rake-update.sh
@@ -1,14 +1,11 @@
-# Change contentConcepts from Associated Concept to Classification
-#psql -U csadmin -d watermill_watermill -f csible/sql-batch/update-concept-refnames.sql
-
 # Update cataloging's Production Place field
-#rake template:places:items:process[imports/places/items.csv,imports/places/payloads]
-#rake cs:post:directory[placeauthorities/urn:cspace:name\(place\)/items,imports/places/payloads,1]
-#rake cs:relate:authorities[placeauthorities/urn:cspace:name\(place\)/items,places,imports/places/relationships.csv]
-#psql -U csadmin -d watermill_watermill -f imports/places/sql/places-refnames.sql
+rake template:places:items:process[imports/places/items.csv,imports/places/payloads]
+rake cs:post:directory[placeauthorities/urn:cspace:name\(place\)/items,imports/places/payloads,1]
+rake cs:relate:authorities[placeauthorities/urn:cspace:name\(place\)/items,places,imports/places/relationships.csv]
+rake template:places:refnames:items:process[imports/places/refnames-items.csv,sql-batch]
 
 # Update catalogins's Material field
 rake template:concepts:items:process[imports/concepts/items.csv,imports/concepts/payloads]
 rake cs:post:directory[conceptauthorities/urn:cspace:name\(material_ca\)/items,imports/concepts/payloads,1]
 rake cs:relate:authorities[conceptauthorities/urn:cspace:name\(material_ca\)/items,concepts,imports/concepts/relationships.csv]
-#psql -U csadmin -d watermill_watermill -f imports/concepts/sql/concepts-refnames.sql
+rake template:concepts:refnames:items:process[imports/concepts/refnames-items.csv,sql-batch]
--- sql-update.sh
+++ sql-update.sh
@@ -1,8 +1,8 @@
 # Updates for the contentConcepts cataloging field
-psql -U csadmin -d watermill_watermill -f csible/sql-batch/update-concept-refnames.sql
+psql -U csadmin -d watermill_watermill -f csible/sql-batch/update-general-subject-terms-refnames.sql
 
 # Updates for the productionPlace cataloging field
-psql -U csadmin -d watermill_watermill -f imports/places/sql/places-refnames.sql
+psql -U csadmin -d watermill_watermill -f csible/sql-batch/places-refnames.sql
 
 # Updates for the material cataloging field
-psql -U csadmin -d watermill_watermill -f imports/concepts/sql/concepts-refnames.sql
+psql -U csadmin -d watermill_watermill -f csible/sql-batch/concepts-refnames.sql
