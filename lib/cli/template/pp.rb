89a3d6b72bf8be6bf41e70d3587161023edfc9e3
Mark Cooper <markchristophercooper@gmail.com>
2016-04-07 12:32:35 -0700

    Simplify namespacing, add placeholder for objects

--- lib/cli/template/pp.rb
+++ lib/cli/template/pp.rb
@@ -7,37 +7,55 @@ namespace :template do
     templates_path = $config[:templates][:templates_path] ||= "templates"
 
     namespace :accessions do
-      namespace :objects do
-        config_file     = "#{templates_path}/pastperfect/accessions/objects.config.csv"
-        mapping_file    = "#{templates_path}/pastperfect/accessions/objects.map.csv"
-        fields          = Csible::CSV.get_config(config_file)
+      config_file     = "#{templates_path}/pastperfect/accessions/accessions.config.csv"
+      mapping_file    = "#{templates_path}/pastperfect/accessions/accessions.map.csv"
+      fields          = Csible::CSV.get_config(config_file)
 
-        fields[:generate] = {
-          ownertype: {
-            from: :accessno,
-            required: false,
-            unique: false,
-            process: ->(value) { 'person' }
-          },
-        }
+      fields[:generate] = {
+        ownertype: {
+          from: :accessno,
+          required: false,
+          unique: false,
+          process: ->(value) { 'person' }
+        },
+      }
 
-        fields[:merge] = {
-          descrip_: :notes_,
-        }
+      fields[:merge] = {
+        descrip_: :notes_,
+      }
 
-        # rake template:pp:accessions:objects:fields
-        desc "Display fields for accessions objects"
-        task :fields do |t|
-          Csible::CSV.print_fields fields[:required], fields[:optional], fields[:generate].keys
-        end
+      # rake template:pp:accessions:fields
+      desc "Display fields for accessions objects"
+      task :fields do |t|
+        Csible::CSV.print_fields fields[:required], fields[:optional], fields[:generate].keys
+      end
 
-        # rake template:pp:accessions:objects:process[templates/pastperfect/accessions/objects.example.csv]
-        desc "Create CollectionSpace accessions CSV from Past Perfect csv"
-        task :process, [:csv, :output_dir] do |t, args|
-          output_dir = args[:output_dir] || output_dir
-          processor = Csible::CSV::PastPerfect.new(args[:csv], output_dir, mapping_file, fields)
-          processor.process
-        end
+      # rake template:pp:accessions:process[templates/pastperfect/accessions/accessions.example.csv]
+      desc "Create CollectionSpace accessions CSV from Past Perfect csv"
+      task :process, [:csv, :output_dir] do |t, args|
+        output_dir = args[:output_dir] || output_dir
+        processor = Csible::CSV::PastPerfect.new(args[:csv], output_dir, mapping_file, fields)
+        processor.process
+      end
+    end
+
+    namespace :objects do
+      config_file     = "#{templates_path}/pastperfect/objects/objects.config.csv"
+      mapping_file    = "#{templates_path}/pastperfect/objects/objects.map.csv"
+      fields          = Csible::CSV.get_config(config_file)
+
+      # rake template:pp:objects:fields
+      desc "Display fields for objects objects"
+      task :fields do |t|
+        Csible::CSV.print_fields fields[:required], fields[:optional], fields[:generate].keys
+      end
+
+      # rake template:pp:objects:process[templates/pastperfect/objects/objects.example.csv]
+      desc "Create CollectionSpace collectionobjects CSV from Past Perfect csv"
+      task :process, [:csv, :output_dir] do |t, args|
+        output_dir = args[:output_dir] || output_dir
+        processor = Csible::CSV::PastPerfect.new(args[:csv], output_dir, mapping_file, fields)
+        processor.process
       end
     end
   end
